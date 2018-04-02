c699e8c6a816885646e7bb00c5462d7b9c3cc07a
watcharaphans <watcharaphans@gmail.com>
2018-04-03 06:13:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,18 +19,17 @@ if (!is_null($events['events'])) {
 		  if ($event['type'] == 'message') { 
 		      switch($event['message']['type']) { 
 			       case 'text': 
-				        // Get replyToken 
-						$replyToken = $event['replyToken']; 
+			             // Get replyToken 
+				     $replyToken = $event['replyToken']; 
 						
+				     // Reply message 
+				     $respMessage = 'Hello, your message is '. $event['message']['text']; 
 						
-						// Reply message 
-						$respMessage = 'Hello, your message is '. $event['message']['text']; 
-						
-						$httpClient = new CurlHTTPClient($channel_token); 
-						$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
-						$textMessageBuilder = new TextMessageBuilder($respMessage); 
-						$response = $bot->replyMessage($replyToken, $textMessageBuilder); 
-					break; 
+				     $httpClient = new CurlHTTPClient($channel_token); 
+				     $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
+				     $textMessageBuilder = new TextMessageBuilder($respMessage); 
+				     $response = $bot->replyMessage($replyToken, $textMessageBuilder); 
+				break; 
 			 } 
 	    } 
 	} 
